01c0ae5d637aa938f43d4ec98ed6594c090b10ca
Alexander Shiryaev <aleksandr-shiryaev-1993@mail.ru>
2025-03-30 12:52:34 +0300

    refactoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { SITE } from './src/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: SITE.url,
+  site: "domain.com",
   integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
-  },
-
-  i18n: {
-    locales: ["ru", "en"],
-    defaultLocale: "en",
-    routing: {
-        prefixDefaultLocale: false
-    }
   }
 });
--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const SITE = {
-    title: 'Мой сайт',
-    description: 'Многоязычный сайт на Astro',
-    defaultLocale: 'ru',
-    url: import.meta.env.PROD 
-      ? 'https://ваш-сайт.com' 
-      : 'http://localhost:3000',
-  };
